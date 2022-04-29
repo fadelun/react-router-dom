40a7a559c104405bfb2994cfc03b6a640f56bf14
MuhammadFadll <fadeljourney@gmail.com>
2022-04-29 10:22:25 +0700

    styled active links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,25 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
+  const styledNavActive = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? "bold" : "normal",
+      textDecoration: isActive ? "none" : "underline",
+      transition: "0.5s",
+    };
+  };
+
+  //   const styledNav = {
+  //     backgroundColor: "cyan",
+  //   };
   return (
     <div className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <NavLink to="/" style={styledNavActive}>
+        Home
+      </NavLink>
+      <NavLink to="/about" style={styledNavActive}>
+        About
+      </NavLink>
     </div>
   );
 };
